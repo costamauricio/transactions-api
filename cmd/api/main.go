91664530b391a6b058f28dfdb30fff6cc08934c1
Maurício Costa <mauriciocosta.cm@gmail.com>
2023-04-30 21:21:35 -0300

    Changed application main to inject the handlers dependencies and orchestrate the database connection.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,38 +2,75 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"github.com/costamauricio/transactions-api/cmd/api/server"
-	"github.com/costamauricio/transactions-api/config"
-	log "github.com/sirupsen/logrus"
+	"github.com/costamauricio/transactions-api/internal/data-access"
+	"github.com/costamauricio/transactions-api/internal/handlers"
+	"github.com/costamauricio/transactions-api/internal/server"
+	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/sirupsen/logrus"
 )
 
+// Get the environment variable value, and when it is not defined returns the default value
+func getEnvWithDefault(env string, defaultValue string) string {
+	value := os.Getenv(env)
+
+	if value == "" {
+		return defaultValue
+	}
+
+	return value
+}
+
 func main() {
-	logger := log.New()
-	logger.SetFormatter(&log.TextFormatter{
+	// Loads the .env file into the environment variables
+	godotenv.Load()
+
+	// Creates a new customized logger using logrus
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
 	})
 
-	env := config.Load()
-	api := server.New(env.Port())
+	// Opens the database connection
+	db, err := sql.Open("sqlite3", getEnvWithDefault("DATABASE_FILE", "transactions.db"))
+	if err != nil {
+		logger.Errorf("Failed to connect to the database: %s", err)
+		return
+	}
+	defer db.Close()
+
+	// Inject the database pool to the daos and inject handlers dependencies
+	container := &handlers.ApplicationHandlers{
+		Logger:        logger,
+		AccountDAO:    &dataAccess.Account{Pool: db},
+		TransactioDAO: &dataAccess.Transaction{Pool: db},
+	}
+
+	port := getEnvWithDefault("PORT", "80")
+
+	// Creates a new Server instance
+	api := server.New(port)
+	api.AttachHandlers(container)
 
 	// Creates a channel to listen for system signals to terminate the aplication
 	terminate := make(chan os.Signal, 1)
 	signal.Notify(terminate, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
-		logger.Infof("Starting server listening at port %v", env.Port())
-		err := api.Listen()
+		logger.Infof("Starting server listening at port %s", port)
+		err := api.StartListening()
 
 		// When shutting down the server, it immediately return a http.ErrServerClosed
 		// so we need to ignore this error when catching errors from the server
 		if err != nil && err != http.ErrServerClosed {
-			logger.Errorf("Failed to start the server: %v", err)
+			logger.Errorf("Failed to start the server: %s", err)
 			// Send a SIGTERM signal to terminate the application
 			terminate <- syscall.SIGTERM
 		}
@@ -49,6 +86,6 @@ func main() {
 	defer cancel()
 
 	if err := api.Shutdown(ctx); err != nil {
-		logger.Fatalf("Error when terminating the server: %v", err)
+		logger.Fatalf("Error when terminating the server: %s", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -10,10 +10,14 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/sirupsen/logrus v1.9.0
+	github.com/stretchr/testify v1.8.2
 )
 
 require (
 	github.com/ajg/form v1.5.1 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
